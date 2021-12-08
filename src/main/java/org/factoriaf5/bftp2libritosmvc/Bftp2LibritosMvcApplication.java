2ffb61d8515a48a6e17b18f946eb5d1c6dd73716
Javi <javi@jsrois.net>
2021-12-08 20:46:10 +0100

    Creando el proyecto con index.html

---
+++ src/main/java/org/factoriaf5/bftp2libritosmvc/Bftp2LibritosMvcApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.bftp2libritosmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bftp2LibritosMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bftp2LibritosMvcApplication.class, args);
+    }
+
+}
